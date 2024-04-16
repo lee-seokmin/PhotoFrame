3d2c5bb6473210276a501c988f1d122cef4413f6
이석민 <68291436+seokmin12@users.noreply.github.com>
2024-04-16 16:47:43 +0900

    Delete CORS

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 import base64
 from fastapi import FastAPI, UploadFile, Form, File, BackgroundTasks
-from fastapi.middleware.cors import CORSMiddleware
 import os
 import uuid
 
@@ -10,24 +9,6 @@ import numpy as np
 import cv2
 
 app = FastAPI()
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 def image_edit(photog, image, filename):
